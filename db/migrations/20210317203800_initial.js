193ba7ec0921e0556b7329a830140dad2ebdad4e
mainlyetcetera <ericcampbell294@gmail.com>
2021-03-17 14:47:05 -0600

    Add footnotes table

--- db/migrations/20210317203800_initial.js
+++ db/migrations/20210317203800_initial.js
@@ -8,7 +8,17 @@ exports.up = function(knex, Promise) {
 
       table.timestamps(true, true)
     }),
-  
+
+    knex.schema.createTable('footnotes', function(table) {
+      table.increments('id').primary()
+      table.string('note')
+      table.integer('paper_id').unsigned()
+      table.foreign('paper_id')
+        .references('papers.id')
+
+      table.timestamps(true, true)
+    })
+  ])
 }
 
 exports.down = function(knex, Promise) {
