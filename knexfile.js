b6a60ae75a23a581863c411561b45290ac38717c
mainlyetcetera <ericcampbell294@gmail.com>
2021-03-17 14:37:17 -0600

    Update migrations

--- knexfile.js
+++ knexfile.js
@@ -2,6 +2,9 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost/publications',
+    migrations: {
+      directory: './db/migrations'
+    },
     useNullAsDefault: true
   }
 }
