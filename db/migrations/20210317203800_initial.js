e7e69e235d846ad2d57f80439ba15ae210660718
mainlyetcetera <ericcampbell294@gmail.com>
2021-03-17 14:46:31 -0600

    Add export up, papers, footnotes tables

--- db/migrations/20210317203800_initial.js
+++ db/migrations/20210317203800_initial.js
@@ -1,7 +1,16 @@
 exports.up = function(knex, Promise) {
+  // what should happen run migration
+  return Promise.all([
+    knex.schema.createTable('papers', function(table) {
+      table.increments('id').primary()
+      table.string('title')
+      table.string('author')
+
+      table.timestamps(true, true)
+    }),
   
 }
 
 exports.down = function(knex, Promise) {
-  
+  // roll back prev version, undo up
 }
