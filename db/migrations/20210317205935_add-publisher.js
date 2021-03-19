2dd0cc17df4900a3ab13c1767a1af15083bafdaa
mainlyetcetera <ericcampbell294@gmail.com>
2021-03-19 16:57:07 -0600

    Remove Promise param, add-publisher

--- db/migrations/20210317205935_add-publisher.js
+++ db/migrations/20210317205935_add-publisher.js
@@ -1,4 +1,4 @@
-exports.up = function(knex, Promise) {
+exports.up = function(knex) {
   return Promise.all([
     knex.schema.table('papers', function(table) {
       table.string('publisher')
@@ -6,7 +6,7 @@ exports.up = function(knex, Promise) {
   ])
 }
 
-exports.down = function(knex, Promise) {
+exports.down = function(knex) {
   return Promise.all([
     knex.schema.table('papers', function(table) {
       table.string('publisher')
