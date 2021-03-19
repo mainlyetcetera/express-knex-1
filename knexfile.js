f096a0f811fb314a3cf55f6b855d1b3a2a167cd8
mainlyetcetera <ericcampbell294@gmail.com>
2021-03-19 16:57:36 -0600

    Add login details, postgres

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,11 @@
 module.exports = {
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/publications',
+    connection: {
+      user: 'postgres',
+      password: 'moneK324',
+      filename: 'postgres://localhost/publications',
+    },
     migrations: {
       directory: './db/migrations'
     },
