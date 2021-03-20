9be842379fbd9586c3fc3f308fed49933c777d3a
mainlyetcetera <ericcampbell294@gmail.com>
2021-03-19 19:49:36 -0600

    Populate server.js

--- server.js
+++ server.js
@@ -11,10 +11,20 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.set('port', process.env.PORT || 3000)
 app.locals.title = 'Publications'
 
-app.get('/', (req, res) => { 
+app.get('/', (req, res) => {
   res.send('Hello, Publications')
 })
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`)
+})
+
+app.get('/api/v1/papers', (req, res) => {
+  database('papers').select()
+    .then(papers => {
+      res.status(200).json(papers)
+    })
+    .catch(error => {
+      res.status(500).json({ error })
+    })
 })
